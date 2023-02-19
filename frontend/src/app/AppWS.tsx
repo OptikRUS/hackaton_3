73ac3168362fe9e77e85e91b0f26a9347f79e06b
Ruslan Ivanov <ruslan4432013@gmail.com>
2023-02-19 09:50:01 +0300

    set ws api

--- frontend/src/app/AppWS.tsx
+++ frontend/src/app/AppWS.tsx
@@ -7,18 +7,24 @@ import { useRootStore } from '@config/store/provider'
 export const AppWS = () => {
   const rootStore = useRootStore()
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8000/exgausters/zsmk-9433-dev-01')
-    console.log('connect')
-    ws.onmessage = (ev) => {
-      const result = JSON.parse(JSON.parse(ev.data)) as WsExgausterMessage
-      console.log(result)
-      rootStore.exhauster_1_u_171.setMessage(result.exgauster_1_u_171)
-      rootStore.exhauster_2_u_172.setMessage(result.exgauster_2_u_172)
-      rootStore.exhauster_3_f_171.setMessage(result.exgauster_3_f_171)
-      rootStore.exhauster_4_f_172.setMessage(result.exgauster_4_f_172)
-      rootStore.exhauster_5_x_171.setMessage(result.exgauster_5_x_171)
-      rootStore.exhauster_6_x_172.setMessage(result.exgauster_6_x_172)
-      rootStore.timeStore.setMoment(result.moment)
+    let active = true
+    if (active) {
+      const ws = new WebSocket('ws://95.163.236.105:8000/exgausters/zsmk-9433-dev-01')
+      console.log('connect')
+      ws.onmessage = (ev) => {
+        const result = JSON.parse(JSON.parse(ev.data)) as WsExgausterMessage
+        console.log(result)
+        rootStore.exhauster_1_u_171.setMessage(result.exgauster_1_u_171)
+        rootStore.exhauster_2_u_172.setMessage(result.exgauster_2_u_172)
+        rootStore.exhauster_3_f_171.setMessage(result.exgauster_3_f_171)
+        rootStore.exhauster_4_f_172.setMessage(result.exgauster_4_f_172)
+        rootStore.exhauster_5_x_171.setMessage(result.exgauster_5_x_171)
+        rootStore.exhauster_6_x_172.setMessage(result.exgauster_6_x_172)
+        rootStore.timeStore.setMoment(result.moment)
+      }
+    }
+    return () => {
+      active = false
     }
   }, [])
   return (
